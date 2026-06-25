c659592ff5a922ee6f6ff1350bfe927de38ea66f
Your Name <you@example.com>
2026-06-25 09:31:09 -0400

    Clean up

--- fix-comma.cjs
+++
@@ -1,5 +0,0 @@
-const fs=require('fs'),p=require('path');
-let f=fs.readFileSync(p.join(__dirname,'index.html'),'utf8');
-f=f.replace('onSwitchView\n  shiftTemplates,','onSwitchView,\n  shiftTemplates,');
-fs.writeFileSync(p.join(__dirname,'index.html'),f,'utf8');
-console.log('OK - comma fixed');
